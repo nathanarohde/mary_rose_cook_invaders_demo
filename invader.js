0619512d6b2d8a2a2e29727ef99da1292ebcb798
Monty Rohde <nathanarohde@gmail.com>
2015-04-14 18:08:14 -0700

    Stopped at 10:21 mark.

---
+++ invader.js
@@ -0,0 +1,76 @@
+;(function() {
+  var Game = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { x: canvas.width, y: canvas.height };
+
+    this.bodies = [new Player(this, gameSize)];
+
+    var self = this;
+    var tick = function() {
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+    };
+
+    tick();
+  };
+
+  Game.prototype = {
+    update: function() {
+
+    },
+
+    draw: function(screen, gameSize) {
+      for (var i=0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i]);
+      }
+    }
+
+  };
+
+  var Player = function(game, gameSize ){
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: gameSize.x /2, y: gameSize.y - this.size.x}
+  };
+
+  Player.prototype = {
+    update: function() {
+
+    }
+  };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y/2,
+                    body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32};
+
+  };
+
+
+
+  window.onload = function() {
+    new Game('screen');
+  };
+
+}) ();
+//https://vimeo.com/105955605 10:21
